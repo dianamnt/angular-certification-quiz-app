54c4feb06911b02d9d7c35bff2948449db4cd732
dianamnt <45576054+dianamnt@users.noreply.github.com>
2023-05-22 18:53:19 +0300

    2 files modified, 1 added

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [CommonModule, BrowserAnimationsModule, MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
+  exports: [MatButtonModule, MatSelectModule],
   declarations: [],
 })
 export class AngularMaterialModule {}
--- src/app/components/quiz-maker/quiz-maker.component.ts
+++ src/app/components/quiz-maker/quiz-maker.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Category } from '../../models/category.model';
+import { QuizService } from '../../services/quiz.service';
+
+enum Difficulty {
+  EASY = 'Easy',
+  MEDIUM = 'Medium',
+  HARD = 'Hard',
+}
 
 @Component({
   selector: 'app-quiz-maker',
   templateUrl: './quiz-maker.component.html',
-  styleUrls: ['./quiz-maker.component.css']
+  styleUrls: ['./quiz-maker.component.css'],
 })
-export class QuizMakerComponent implements OnInit {
+export class QuizMakerComponent implements OnInit, OnDestroy {
+  categories: Category[];
+  quizForm = new FormGroup({
+    category: new FormControl<Category | null>(null, Validators.required),
+    difficulty: new FormControl<string | null>(null, Validators.required),
+  });
+  readonly Difficulty = Difficulty;
+  subscription: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private quizService: QuizService) {}
 
   ngOnInit() {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.subscription.add(
+      this.quizService.getAllCategories().subscribe((data) => {
+        this.categories = data.trivia_categories;
+        console.log(this.categories);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
