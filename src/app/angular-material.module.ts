7fbabfbdc8bd3417a9b40e7c2197a28036d621f4
dianamnt <45576054+dianamnt@users.noreply.github.com>
2023-05-22 17:05:02 +0300

    added angular material

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  imports: [CommonModule, BrowserAnimationsModule, MatButtonModule],
+  exports: [MatButtonModule],
+  declarations: [],
+})
+export class AngularMaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularMaterialModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
