649200e5f6416f9957025ffc476d3c5ff48c66b2
dianamnt <45576054+dianamnt@users.noreply.github.com>
2023-05-24 14:14:49 +0300

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularMaterialModule } from './angular-material.module';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { QuizMakerComponent } from './components/quiz-maker/quiz-maker.component';
 import { ResultsComponent } from './components/results/results.component';
